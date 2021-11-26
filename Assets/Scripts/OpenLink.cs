142bcf81c4dcfbf6fee4bbb98a861eec2e2f7f19
TobiballaTUD <91211382+TobiballaTUD@users.noreply.github.com>
2021-11-26 21:35:42 +0000

    Update OpenLink.cs

--- Assets/Scripts/OpenLink.cs
+++ Assets/Scripts/OpenLink.cs
@@ -6,6 +6,6 @@ public class OpenLink : MonoBehaviour
 {
     public void OpenChannel()
     {
-        Application.OpenURL("https://www.bing.com/images/search?q=create+an+image&form=HDRSC3&first=1&tsc=ImageBasicHover");
+        Application.OpenURL("https://github.com/TobiballaTUD/Meteor");
     }
 }
